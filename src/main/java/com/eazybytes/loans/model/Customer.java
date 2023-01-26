b3685262fa5c85f05a00e7ad30e1c71f6c839072
wahabgithub <52493137+wahabgithub@users.noreply.github.com>
2023-01-26 22:27:54 +0600

    Add files via upload

---
+++ src/main/java/com/eazybytes/loans/model/Customer.java
@@ -0,0 +1,18 @@
+package com.eazybytes.loans.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter @Setter @ToString
+public class Customer {
+
+	private int customerId;
+
+	public int getCustomerId() {
+		// TODO Auto-generated method stub
+		return customerId;
+	}
+
+}
